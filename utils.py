9f1a1740226b8c69f67f770eb20b2516e657f37e
alex <amfrostgm@gmail.com>
2021-03-06 08:25:28 +0000

    perm 2

--- utils.py
+++ utils.py
@@ -199,7 +199,7 @@ if __name__ == '__main__':
 
     # create_permutation()
 
-    make_permutation_live('./questions/permutations/perm_1')
+    # make_permutation_live('./questions/permutations/perm_2')
 
 
 
